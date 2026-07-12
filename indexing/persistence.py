b3e4408547fa662a03f321c937fd08fec87ac551
kaushikmak <kaushikmak35@gmail.com>
2026-07-12 23:26:08 +0530

    [fix] ensure target directory exists before saving graph pickle file

--- indexing/persistence.py
+++ indexing/persistence.py
@@ -1,10 +1,12 @@
 import pickle
 import logging
 import app.config as config
+import os
 
 logger = logging.getLogger(__name__)
 
 def save_graph(graph):
+    os.makedirs(os.path.dirname(config.GRAPH_PATH), exist_ok=True)
     logger.info("Saving graph index")
     with open(config.GRAPH_PATH,"wb") as f:
         pickle.dump(graph, f)
